cc0008b5b4a3d43571e360a80d7bf5ba76aaa686
Rabiussany <muhammad.rabiussany@metlife.com.bd>
2023-09-05 10:38:24 +0600

    new Ad Login redirect and cookies has been implement

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -10,13 +10,19 @@ import { Navigate } from "react-router-dom";
 import TextInput from "../../components/inputs/TextInput";
 import { ReloadOutlined } from "@ant-design/icons";
 import Loader from "../../components/loader/Loader";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
 
 const LoginPage = () => {
+  const azureUrl =
+    "https://login.microsoftonline.com/ca56a4a5-e300-406a-98ff-7e36a0baac5b/oauth2/v2.0/authorize?client_id=5a6f192a-8f85-4f86-8715-2efa16a9ea41&response_type=code&redirect_uri=https%3a%2f%2fdev.ulm.metlife.com.bd%2f&response_mode=query&scope=https%3a%2f%2fgraph.microsoft.com%2fuser.read";
   const [isLoading, setLoading] = useState(false);
   const [errorMessage, setErrorMassage] = useState("");
   const urlParams = new URLSearchParams(window.location.search);
   const code = urlParams.get("code");
+  const session_state = urlParams.get("session_state");
+  const navigate = useNavigate()
+  console.log("document.cookies", code);
 
   const token = localStorage.getItem("access-token");
   if (token !== null) {
@@ -27,15 +33,23 @@ const LoginPage = () => {
     authorizationCode: code,
   };
 
+  function deleteCookie(name) {
+    document.cookie =
+      name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  }
+
   // Api Calling ----------
 
   useEffect(() => {
+
+    if ( code ) {
+
     (async () => {
-      
-      if (payload) {
-        try {
-          setLoading(true);
-          const res = await userLogin(payload);
+      try {
+        setLoading(true);
+        const res = await userLogin(payload);
+        console.log("res", res)
+        if (res.data.status === false) {
           const token = res.data.data.token;
           const username = res.data.data.username;
           localStorage.setItem("access-token", token);
@@ -45,16 +59,27 @@ const LoginPage = () => {
           window.location.href = "/";
           message.success(res.data.message);
           setLoading(false);
-        } catch (error) {
-          console.log(error);
-          message.error("Something went wrong !");
-          setLoading(false);
-          // error?.response?.data?.details[0] &&
-          //   message.error(error?.response?.data?.details[0]);
+          return;
         }
+
+        // window.location.href = azureUrl;
+      } catch (error) {
+        console.log("error", error);
+        setLoading(false);
+        deleteCookie(code);
+        deleteCookie(session_state);
+
+        console.log("azureUrl", azureUrl)
+        navigate(azureUrl)
+        // window.location.href = azureUrl;
+
+        // error?.response?.data?.details[0] &&
+        //   message.error(error?.response?.data?.details[0]);
       }
     })();
-  }, []);
+  }
+
+  }, [code]);
 
   return (
     <>
@@ -68,7 +93,7 @@ const LoginPage = () => {
                 <img src={logo} width={120} alt="logo" />
               </div>
               <div className="sign_text">
-                <h6> Redirecting to ULM Dashboard</h6>
+                <h6> Redirecting to Login</h6>
                 <ReloadOutlined spin /> ...
               </div>
             </div>
