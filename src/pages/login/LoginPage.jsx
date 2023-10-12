2bc5387e673597b71f4988d9cd5fdc36e9a1d6d1
Rabiussany <muhammad.rabiussany@metlife.com.bd>
2023-10-12 13:17:22 +0600

    Login Issues are back on prev state

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -11,7 +11,8 @@ import Loader from "../../components/loader/Loader";
 const LoginPage = () => {
   const azureLogin =
     "https://login.microsoftonline.com/ca56a4a5-e300-406a-98ff-7e36a0baac5b/oauth2/v2.0/authorize?client_id=5a6f192a-8f85-4f86-8715-2efa16a9ea41&response_type=code&redirect_uri=https%3a%2f%2fdev.ulm.metlife.com.bd%2f&response_mode=query&scope=https%3a%2f%2fgraph.microsoft.com%2fuser.read";
-
+  // const azureLogoutUrl =
+  //   "https://login.microsoftonline.com/common/oauth2/logout?post_logout_redirect_uri=https%3A%2F%2Flogin.microsoftonline.com%2Fca56a4a5-e300-406a-98ff-7e36a0baac5b%2Foauth2%2Fv2.0%2Fauthorize%3Fclient_id%3D5a6f192a-8f85-4f86-8715-2efa16a9ea41%26response_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Fdev.ulm.metlife.com.bd%252F%26response_mode%3Dquery%26scope%3Dhttps%253A%252F%252Fgraph.microsoft.com%252Fuser.read%26sso_reload%3Dtrue";
   const azureLogoutUrl = `https://login.microsoftonline.com/ca56a4a5-e300-406a-98ff-7e36a0baac5b/oauth2/v2.0/logout?post_logout_redirect_uri=${azureLogin}`;
 
   const urlParams = new URLSearchParams(window.location.search);
@@ -41,14 +42,13 @@ const LoginPage = () => {
             setStatus(200);
             const token = res.data.data.token;
             const username = res.data.data.username;
-            const role = res.data.data.authority;
-            const expiration = res.data.data.expiration;
+            const role = res.data.data.authority?.authority[0];
+            console.log("role",role)
             localStorage.setItem("access-token", token);
             const user = jwt_decode(token);
             localStorage.setItem("user", JSON.stringify(user));
             localStorage.setItem("username", JSON.stringify(username));
             localStorage.setItem("role", JSON.stringify(role));
-            localStorage.setItem("expiration", JSON.stringify(expiration));
             window.location.href = "/dashboard";
             message.success(res.data.message);
             setLoading(false);
@@ -57,8 +57,7 @@ const LoginPage = () => {
         } catch (error) {
           setLoading(false);
           setTimeout(() => {
-            error?.response?.data?.details[0] &&
-              message.error(error?.response?.data?.details[0]);
+            error?.response?.data?.details[0] && message.error(error?.response?.data?.details[0]);
             window.location.reload();
             window.location.href = azureLogoutUrl;
           }, 2000);
