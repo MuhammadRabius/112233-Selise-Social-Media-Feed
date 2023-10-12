62b622eadda9b7efe061b7fbedb7903d9836eab6
Rabiussany <muhammad.rabiussany@metlife.com.bd>
2023-10-12 11:16:25 +0600

    api endp point changes on Lad service lead status

--- src/pages/Report/Service/Report_Service.js
+++ src/pages/Report/Service/Report_Service.js
@@ -1,15 +1,26 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const getReport = async ({fromDate, toDate,leadSourceId,email,phoneNumber} , resType) => {
-
-    return await axios.get(`api/leads/reports?fromDate=${fromDate}&toDate=${toDate}&leadSourceId=${leadSourceId}&email=${email}&phoneNumber=${phoneNumber}`,resType);
-}
-export const getCount = async ({fromDate, toDate,leadSourceId,email,phoneNumber} ) => {
-
-    return await axios.get(`/api/leads/counts?fromDate=${fromDate}&toDate=${toDate}&leadSourceId=${leadSourceId}&email=${email}&phoneNumber=${phoneNumber}`);
-}
+export const getReport = async (
+  { fromDate, toDate, leadSourceId, email, phoneNumber },
+  resType
+) => {
+  return await axios.get(
+    `api/leads/reports?fromDate=${fromDate}&toDate=${toDate}&leadSourceId=${leadSourceId}&email=${email}&phoneNumber=${phoneNumber}`,
+    resType
+  );
+};
+export const getCount = async ({
+  fromDate,
+  toDate,
+  leadSourceId,
+  email,
+  phoneNumber,
+}) => {
+  return await axios.get(
+    `/api/leads/counts?fromDate=${fromDate}&toDate=${toDate}&leadSourceId=${leadSourceId}&email=${email}&phoneNumber=${phoneNumber}`
+  );
+};
 
 export const getSource = async () => {
-
-    return await axios.get(`/api/leads/sources`);
-}
+  return await axios.get(`/api/lead-sources`);
+};
