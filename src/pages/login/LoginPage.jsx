833c3fbde752634a97ca2be56aca87a17b125a11
Rabiussany <muhammad.rabiussany@metlife.com.bd>
2023-10-26 14:27:14 +0600

    azurelogout try old system in page

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -6,10 +6,15 @@ import { message } from "antd";
 import { Navigate } from "react-router-dom";
 import { ReloadOutlined } from "@ant-design/icons";
 import Loader from "../../components/loader/Loader";
-import { azureLogin, azureLogoutUrl } from "../../utility/Urls";
+// import { azureLogin, azureLogoutUrl } from "../../utility/Urls";
 import { userLogin } from "../../services/Services";
 
 const LoginPage = () => {
+  const azureLogin =
+    "https://login.microsoftonline.com/ca56a4a5-e300-406a-98ff-7e36a0baac5b/oauth2/v2.0/authorize?client_id=5a6f192a-8f85-4f86-8715-2efa16a9ea41&response_type=code&redirect_uri=https%3a%2f%2fdev.ulm.metlife.com.bd%2f&response_mode=query&scope=https%3a%2f%2fgraph.microsoft.com%2fuser.read";
+
+  const azureLogoutUrl = `https://login.microsoftonline.com/ca56a4a5-e300-406a-98ff-7e36a0baac5b/oauth2/v2.0/logout?post_logout_redirect_uri=${azureLogin}`;
+
   const urlParams = new URLSearchParams(window.location.search);
   const code = urlParams.get("code");
   const [isLoading, setLoading] = useState(false);
