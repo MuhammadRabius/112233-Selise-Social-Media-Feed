581c5cb59d9f3142c5cb9476b3372763838c5051
Rabiussany <muhammad.rabiussany@metlife.com.bd>
2023-11-09 16:07:42 +0600

    base url has been change api to apim and login sso ,logout slo has been inseative for testing

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -12,8 +12,10 @@ import { userLogin } from "../../services/Services";
 const LoginPage = () => {
   const azureLogin =
     "https://login.microsoftonline.com/ca56a4a5-e300-406a-98ff-7e36a0baac5b/oauth2/v2.0/authorize?client_id=5a6f192a-8f85-4f86-8715-2efa16a9ea41&response_type=code&redirect_uri=https%3a%2f%2fdev.ulm.metlife.com.bd%2f&response_mode=query&scope=https%3a%2f%2fgraph.microsoft.com%2fuser.read";
-
+  // const azureLogoutUrl =
+  //   "https://login.microsoftonline.com/common/oauth2/logout?post_logout_redirect_uri=https%3A%2F%2Flogin.microsoftonline.com%2Fca56a4a5-e300-406a-98ff-7e36a0baac5b%2Foauth2%2Fv2.0%2Fauthorize%3Fclient_id%3D5a6f192a-8f85-4f86-8715-2efa16a9ea41%26response_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Fdev.ulm.metlife.com.bd%252F%26response_mode%3Dquery%26scope%3Dhttps%253A%252F%252Fgraph.microsoft.com%252Fuser.read%26sso_reload%3Dtrue";
   const azureLogoutUrl = `https://login.microsoftonline.com/ca56a4a5-e300-406a-98ff-7e36a0baac5b/oauth2/v2.0/logout?post_logout_redirect_uri=${azureLogin}`;
+
 
   const urlParams = new URLSearchParams(window.location.search);
   const code = urlParams.get("code");
