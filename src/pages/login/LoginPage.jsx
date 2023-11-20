209e86966d37ff14a6302e2af7947709d1cb98f7
Rabiussany <muhammad.rabiussany@metlife.com.bd>
2023-11-20 23:47:26 +0600

    redirect qa after login and logout another approch init

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -12,9 +12,9 @@ import { userLogin } from "../../services/Services";
 const LoginPage = () => {
   const azureLogin =
   "https://login.microsoftonline.com/ca56a4a5-e300-406a-98ff-7e36a0baac5b/oauth2/v2.0/authorize?client_id=7c2a7e4f-2fc8-463a-be17-acdc93f37b92&response_type=code&redirect_uri=https%3a%2f%2fqa.ulm.metlife.com.bd%2f&response_mode=query&scope=https%3a%2f%2fgraph.microsoft.com%2fuser.read";
-  const azureLogoutUrl =
-    "https://login.microsoftonline.com/common/oauth2/logout?post_logout_redirect_uri=https%3A%2F%2Flogin.microsoftonline.com%2Fca56a4a5-e300-406a-98ff-7e36a0baac5b%2Foauth2%2Fv2.0%2Fauthorize%3Fclient_id%3D5a6f192a-8f85-4f86-8715-2efa16a9ea41%26response_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Fdev.ulm.metlife.com.bd%252F%26response_mode%3Dquery%26scope%3Dhttps%253A%252F%252Fgraph.microsoft.com%252Fuser.read%26sso_reload%3Dtrue";
-  // const azureLogoutUrl = `https://login.microsoftonline.com/ca56a4a5-e300-406a-98ff-7e36a0baac5b/oauth2/v2.0/logout?post_logout_redirect_uri=${azureLogin}`;
+  // const azureLogoutUrl =
+    // "https://login.microsoftonline.com/oauth2/v2.0/logout?post_logout_redirect_uri=https%3A%2F%2Flogin.microsoftonline.com%2Fca56a4a5-e300-406a-98ff-7e36a0baac5b%2Foauth2%2Fv2.0%2Fauthorize%3Fclient_id%7c2a7e4f-2fc8-463a-be17-acdc93f37b92%26response_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Fqa.ulm.metlife.com.bd%252F%26response_mode%3Dquery%26scope%3Dhttps%253A%252F%252Fgraph.microsoft.com%252Fuser.read%26sso_reload%3Dtrue";
+  const azureLogoutUrl = `https://login.microsoftonline.com/ca56a4a5-e300-406a-98ff-7e36a0baac5b/oauth2/v2.0/logout?post_logout_redirect_uri=https%3a%2f%2fqa.ulm.metlife.com.bd%2f`;
 
 
   const urlParams = new URLSearchParams(window.location.search);
