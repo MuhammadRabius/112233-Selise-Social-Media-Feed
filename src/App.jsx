83317509b6f3b598a7f2c6b35e69090d5b88704a
MuhammadRabius <muhammadrabiussany@gamil.com>
2024-09-05 12:23:21 +0600

    Initlize the project of Social Media - Feed

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,18 @@
 import React from "react";
 import {Routes, Route } from "react-router-dom";
-import HomePage from "./pages/home/HomePage";
-import LeadsPage from "./pages/leads/LeadsPage";
-import LoginPage from "./pages/login/LoginPage";
-import UserManagement from "./pages/user-management/UserManagement";
-import Report from "./pages/Report/Report";
-import RequireAuth from "./components/auth/RequireAuth";
+import MediaFeed from "./pages/HomePage/MediaFeed";
 
 
 
 const App = () => {
   return (
-    <>
+  
       <Routes>
         
-        <Route path="/" element={<LoginPage />} />
-        <Route element={<RequireAuth />}>
-          <Route path="/dashboard" element={<HomePage />} />
-          <Route path="/leads" element={<LeadsPage />} />
-          <Route path="/usermanagement" element={<UserManagement />} />
-          <Route path="/report" element={<Report />} />
-        </Route>
+        <Route path="/" element={<MediaFeed />} />
+       
       </Routes>
-    </>
+    
   );
 };
 
---
+++ src/pages/HomePage/MediaFeed.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MediaFeed =()=> {
+  return (
+    <div>MediaFeed</div>
+  )
+}
+
+export default MediaFeed
